baa4b12f435e4b1fa890969b06f6598b7105b156
Lionel G. <55513844+rezopress@users.noreply.github.com>
2026-02-02 13:13:38 -0500

    Création des états

--- Labo01/labo1.ino
+++ Labo01/labo1.ino
@@ -13,4 +13,42 @@ void setup() {
 
 void loop() {
 
+  // =========================
+  // ÉTAT : ALLUMÉ / ÉTEINT
+  // =========================
+  Serial.println("Etat : Allume – 6357132");
+
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(300);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(2000);
+
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000);
+
+  // =========================
+  // ÉTAT : VARIATION D'INTENSITÉ
+  // =========================
+  Serial.println("Etat : Varie – 6357132");
+
+  // Dernier chiffre paire donc 0 à 255
+  for (int i = 0; i <= 255; i++) {
+    analogWrite(LED_BUILTIN, i);
+    delay(8);  // Durée total 2048ms = 2048 / 256 = 8ms
+  }
+
+  // =========================
+  // ÉTAT : CLIGNOTEMENT
+  // =========================
+  Serial.println("Etat : Clignotement – 6357132");
+
+  for (int i = 0; i < 2; i++) {   // 2 clignotements ( 3/2 = 1.5 -> plafond = 2 )
+
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(350);
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(350);
+  }
+
 }
